63427c7ae3b92b75ba1982d06d05ec53e9349cea
Deepak-Verma-556 <deepakkhudia46015@gmail.com>
2024-10-19 12:43:26 +0530

    add gradient

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-      <section>
+      <section className='bg-gradient-to-r from-[#474747] to-[#222222]'>
           <nav>
-              <div className='max-w-xxl m-auto px-4'>
+              <div className='max-w-xxl mx-auto px-4'>
                   <div>
                       <a href="">General <br/> <span>Project</span></a>
                   </div>
+                  <ul>
+                      <li></li>
+                  </ul>
               </div>
           </nav>
     </section>
