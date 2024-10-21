081392e602267c5c5bb306700f52df1cf6e8ef51
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-21 11:50:49 +0530

    add respnsive width

--- src/components/Make.jsx
+++ src/components/Make.jsx
@@ -22,7 +22,7 @@ const Make = () => {
                         your company and don't forget to ask via our email or our
                         customer service if you are interested in using our services</p>
                 </div>
-                <div className='flex flex-wrap flex-row -mx-3 pt-16'>
+                <div className='flex flex-wrap flex-row -mx-3 pt-16 max-sm:pt-8'>
                     <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 max-sm:text-center'>
                         <div className='max-sm:flex max-sm:justify-center'>
                             <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center '>
@@ -33,49 +33,55 @@ const Make = () => {
                                 </div>
                             </div>
                         </div>
-                        <h2 className='text-2xl font-semibold py-6 max-sm:py-4'>Experience</h2>
-                        <p className='font-medium text-lg leading-7 text-gray'>Our experience of 25 years of building and making achievements in the world of development</p>
+                        <h2 className='text-2xl font-semibold py-6 max-sm:py-4 max-sm:text-xl'>Experience</h2>
+                        <p className='font-medium text-lg leading-7 text-gray max-sm:text-base'>Our experience of 25 years of building and making achievements in the world of development</p>
                     </div>
                     <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 max-sm:mt-4 max-sm:text-center'>
-                        <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center'>
-                            <div className='w-[74px] h-[74px] bg-dark-blue rounded-full flex items-center justify-center'>
-                                <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M29.7529 3.28125H21.3516C21.0903 3.28106 20.8397 3.38428 20.6543 3.56836L3.8584 20.3643C3.49064 20.7338 3.28418 21.234 3.28418 21.7554C3.28418 22.2768 3.49064 22.7769 3.8584 23.1465L11.8565 31.1445C12.226 31.5123 12.7262 31.7188 13.2476 31.7188C13.7689 31.7188 14.2691 31.5123 14.6387 31.1445L31.4277 14.3555C31.6118 14.1701 31.715 13.9194 31.7149 13.6582V5.25C31.7161 4.99168 31.6663 4.73567 31.5682 4.49668C31.4702 4.2577 31.3258 4.04047 31.1435 3.85749C30.9611 3.67451 30.7444 3.5294 30.5057 3.43051C30.2671 3.33161 30.0113 3.28089 29.7529 3.28125V3.28125Z" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M26.25 10.9375C25.8174 10.9375 25.3944 10.8092 25.0347 10.5688C24.675 10.3285 24.3946 9.98683 24.229 9.58712C24.0634 9.18741 24.0201 8.74757 24.1045 8.32324C24.1889 7.89891 24.3973 7.50913 24.7032 7.20321C25.0091 6.89728 25.3989 6.68894 25.8232 6.60453C26.2476 6.52013 26.6874 6.56345 27.0871 6.72901C27.4868 6.89458 27.8285 7.17496 28.0688 7.53469C28.3092 7.89442 28.4375 8.31735 28.4375 8.75C28.4375 9.33016 28.207 9.88656 27.7968 10.2968C27.3866 10.707 26.8302 10.9375 26.25 10.9375Z" fill="white" />
-                                </svg>
+                        <div className='max-sm:flex max-sm:justify-center'>
+                            <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center'>
+                                <div className='w-[74px] h-[74px] bg-dark-blue rounded-full flex items-center justify-center'>
+                                    <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M29.7529 3.28125H21.3516C21.0903 3.28106 20.8397 3.38428 20.6543 3.56836L3.8584 20.3643C3.49064 20.7338 3.28418 21.234 3.28418 21.7554C3.28418 22.2768 3.49064 22.7769 3.8584 23.1465L11.8565 31.1445C12.226 31.5123 12.7262 31.7188 13.2476 31.7188C13.7689 31.7188 14.2691 31.5123 14.6387 31.1445L31.4277 14.3555C31.6118 14.1701 31.715 13.9194 31.7149 13.6582V5.25C31.7161 4.99168 31.6663 4.73567 31.5682 4.49668C31.4702 4.2577 31.3258 4.04047 31.1435 3.85749C30.9611 3.67451 30.7444 3.5294 30.5057 3.43051C30.2671 3.33161 30.0113 3.28089 29.7529 3.28125V3.28125Z" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M26.25 10.9375C25.8174 10.9375 25.3944 10.8092 25.0347 10.5688C24.675 10.3285 24.3946 9.98683 24.229 9.58712C24.0634 9.18741 24.0201 8.74757 24.1045 8.32324C24.1889 7.89891 24.3973 7.50913 24.7032 7.20321C25.0091 6.89728 25.3989 6.68894 25.8232 6.60453C26.2476 6.52013 26.6874 6.56345 27.0871 6.72901C27.4868 6.89458 27.8285 7.17496 28.0688 7.53469C28.3092 7.89442 28.4375 8.31735 28.4375 8.75C28.4375 9.33016 28.207 9.88656 27.7968 10.2968C27.3866 10.707 26.8302 10.9375 26.25 10.9375Z" fill="white" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
-                        <h2 className='text-2xl font-semibold py-6'>competitive price</h2>
-                        <p className='font-medium text-lg leading-7 text-gray'>The prices we offer you are
+                        <h2 className='text-2xl font-semibold py-6 max-sm:py-4 max-sm:text-xl'>competitive price</h2>
+                        <p className='font-medium text-lg leading-7 text-gray max-sm:text-base'>The prices we offer you are
                             very competitive without
                             reducing the quality of the
                             company's work in the
                             slightest</p>
                     </div>
-                    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 max-md:mt-4'>
-                        <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center'>
-                            <div className='w-[74px] h-[74px] bg-dark-blue rounded-full flex items-center justify-center'>
-                                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M40.6053 17.7031H29.6053C29.4162 17.7031 29.2615 17.8578 29.2615 18.0469V20.1094C29.2615 20.2984 29.4162 20.4531 29.6053 20.4531H40.6053C40.7943 20.4531 40.949 20.2984 40.949 20.1094V18.0469C40.949 17.8578 40.7943 17.7031 40.6053 17.7031ZM34.8475 23.5469H29.6053C29.4162 23.5469 29.2615 23.7016 29.2615 23.8906V25.9531C29.2615 26.1422 29.4162 26.2969 29.6053 26.2969H34.8475C35.0365 26.2969 35.1912 26.1422 35.1912 25.9531V23.8906C35.1912 23.7016 35.0365 23.5469 34.8475 23.5469ZM20.5088 13.8574H18.6482C18.3818 13.8574 18.167 14.0723 18.167 14.3387V24.9949C18.167 25.1496 18.24 25.2914 18.3646 25.3816L24.7627 30.048C24.9775 30.2027 25.2783 30.1598 25.433 29.9449L26.5373 28.4367V28.4324C26.692 28.2176 26.6447 27.9168 26.4299 27.7621L20.9857 23.8262V14.3387C20.99 14.0723 20.7709 13.8574 20.5088 13.8574Z" fill="white" />
-                                    <path d="M34.5813 28.9566H32.0977C31.8571 28.9566 31.6293 29.0812 31.5004 29.2874C30.9547 30.1511 30.3188 30.9503 29.5883 31.6808C28.3293 32.9398 26.8641 33.9281 25.2356 34.6156C23.5469 35.3288 21.7551 35.6898 19.9074 35.6898C18.0555 35.6898 16.2637 35.3288 14.5793 34.6156C12.9508 33.9281 11.4856 32.9398 10.2266 31.6808C8.96761 30.4218 7.97933 28.9566 7.29183 27.3281C6.57855 25.6437 6.21761 23.8519 6.21761 21.9999C6.21761 20.148 6.57855 18.3605 7.29183 16.6718C7.97933 15.0433 8.96761 13.5781 10.2266 12.3191C11.4856 11.0601 12.9508 10.0718 14.5793 9.38432C16.2637 8.67104 18.0598 8.3101 19.9074 8.3101C21.7594 8.3101 23.5512 8.67104 25.2356 9.38432C26.8641 10.0718 28.3293 11.0601 29.5883 12.3191C30.3188 13.0496 30.9547 13.8488 31.5004 14.7124C31.6293 14.9187 31.8571 15.0433 32.0977 15.0433H34.5813C34.8778 15.0433 35.0668 14.7339 34.9336 14.4718C32.1321 8.89877 26.4516 5.27651 20.1094 5.20346C10.8239 5.08744 3.11097 12.6886 3.09378 21.9656C3.07659 31.2597 10.609 38.8007 19.9032 38.8007C26.327 38.8007 32.102 35.1656 34.9336 29.5281C35.0668 29.266 34.8735 28.9566 34.5813 28.9566Z" fill="white" />
-                                </svg>
+                    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 max-md:mt-4 max-sm:text-center'>
+                        <div className='max-sm:flex max-sm:justify-center'>
+                            <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center'>
+                                <div className='w-[74px] h-[74px] bg-dark-blue rounded-full flex items-center justify-center'>
+                                    <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M40.6053 17.7031H29.6053C29.4162 17.7031 29.2615 17.8578 29.2615 18.0469V20.1094C29.2615 20.2984 29.4162 20.4531 29.6053 20.4531H40.6053C40.7943 20.4531 40.949 20.2984 40.949 20.1094V18.0469C40.949 17.8578 40.7943 17.7031 40.6053 17.7031ZM34.8475 23.5469H29.6053C29.4162 23.5469 29.2615 23.7016 29.2615 23.8906V25.9531C29.2615 26.1422 29.4162 26.2969 29.6053 26.2969H34.8475C35.0365 26.2969 35.1912 26.1422 35.1912 25.9531V23.8906C35.1912 23.7016 35.0365 23.5469 34.8475 23.5469ZM20.5088 13.8574H18.6482C18.3818 13.8574 18.167 14.0723 18.167 14.3387V24.9949C18.167 25.1496 18.24 25.2914 18.3646 25.3816L24.7627 30.048C24.9775 30.2027 25.2783 30.1598 25.433 29.9449L26.5373 28.4367V28.4324C26.692 28.2176 26.6447 27.9168 26.4299 27.7621L20.9857 23.8262V14.3387C20.99 14.0723 20.7709 13.8574 20.5088 13.8574Z" fill="white" />
+                                        <path d="M34.5813 28.9566H32.0977C31.8571 28.9566 31.6293 29.0812 31.5004 29.2874C30.9547 30.1511 30.3188 30.9503 29.5883 31.6808C28.3293 32.9398 26.8641 33.9281 25.2356 34.6156C23.5469 35.3288 21.7551 35.6898 19.9074 35.6898C18.0555 35.6898 16.2637 35.3288 14.5793 34.6156C12.9508 33.9281 11.4856 32.9398 10.2266 31.6808C8.96761 30.4218 7.97933 28.9566 7.29183 27.3281C6.57855 25.6437 6.21761 23.8519 6.21761 21.9999C6.21761 20.148 6.57855 18.3605 7.29183 16.6718C7.97933 15.0433 8.96761 13.5781 10.2266 12.3191C11.4856 11.0601 12.9508 10.0718 14.5793 9.38432C16.2637 8.67104 18.0598 8.3101 19.9074 8.3101C21.7594 8.3101 23.5512 8.67104 25.2356 9.38432C26.8641 10.0718 28.3293 11.0601 29.5883 12.3191C30.3188 13.0496 30.9547 13.8488 31.5004 14.7124C31.6293 14.9187 31.8571 15.0433 32.0977 15.0433H34.5813C34.8778 15.0433 35.0668 14.7339 34.9336 14.4718C32.1321 8.89877 26.4516 5.27651 20.1094 5.20346C10.8239 5.08744 3.11097 12.6886 3.09378 21.9656C3.07659 31.2597 10.609 38.8007 19.9032 38.8007C26.327 38.8007 32.102 35.1656 34.9336 29.5281C35.0668 29.266 34.8735 28.9566 34.5813 28.9566Z" fill="white" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
-                        <h2 className='text-2xl font-semibold py-6'>On Time</h2>
-                        <p className='font-medium text-lg leading-7 text-gray'>We prioritize the quality of
+                        <h2 className='text-2xl font-semibold py-6 max-sm:py-4 max-sm:text-xl'>On Time</h2>
+                        <p className='font-medium text-lg leading-7 text-gray max-sm:text-base'>We prioritize the quality of
                             our work and finish it on time</p>
                     </div>
-                    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 max-lg:mt-4'>
-                        <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center'>
-                            <div className='w-[74px] h-[74px] bg-dark-blue rounded-full flex items-center justify-center'>
-                                <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M33.242 10.925C33.2156 10.6589 33.1222 10.4039 32.9705 10.1837C32.8187 9.96355 32.6136 9.78551 32.3743 9.66624L19.7076 3.33291C19.4876 3.22284 19.2451 3.16553 18.9991 3.16553C18.7531 3.16553 18.5105 3.22284 18.2905 3.33291L5.62387 9.66624C5.38522 9.78619 5.18071 9.96445 5.02931 10.1845C4.87791 10.4045 4.78452 10.6592 4.75779 10.925C4.74037 11.0944 3.2362 27.9727 18.357 34.6972C18.5591 34.7883 18.7782 34.8355 18.9999 34.8355C19.2215 34.8355 19.4407 34.7883 19.6427 34.6972C34.7635 27.9727 33.2594 11.096 33.242 10.925ZM18.9999 31.5036C8.28229 26.3229 7.77562 15.2665 7.8627 12.0887L18.9999 6.52016L30.1291 12.0856C30.1877 15.2348 29.6098 26.3641 18.9999 31.5036Z" fill="white" />
-                                    <path d="M17.4163 19.9281L13.7857 16.2975L11.5469 18.5364L17.4163 24.4058L26.4524 15.3697L24.2135 13.1309L17.4163 19.9281Z" fill="white" />
-                                </svg>
+                    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 max-lg:mt-4 max-sm:text-center'>
+                        <div className='max-sm:flex max-sm:justify-center'>
+                            <div className='w-[94px] h-[94px] bg-bluee rounded-full flex items-center justify-center'>
+                                <div className='w-[74px] h-[74px] bg-dark-blue rounded-full flex items-center justify-center'>
+                                    <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M33.242 10.925C33.2156 10.6589 33.1222 10.4039 32.9705 10.1837C32.8187 9.96355 32.6136 9.78551 32.3743 9.66624L19.7076 3.33291C19.4876 3.22284 19.2451 3.16553 18.9991 3.16553C18.7531 3.16553 18.5105 3.22284 18.2905 3.33291L5.62387 9.66624C5.38522 9.78619 5.18071 9.96445 5.02931 10.1845C4.87791 10.4045 4.78452 10.6592 4.75779 10.925C4.74037 11.0944 3.2362 27.9727 18.357 34.6972C18.5591 34.7883 18.7782 34.8355 18.9999 34.8355C19.2215 34.8355 19.4407 34.7883 19.6427 34.6972C34.7635 27.9727 33.2594 11.096 33.242 10.925ZM18.9999 31.5036C8.28229 26.3229 7.77562 15.2665 7.8627 12.0887L18.9999 6.52016L30.1291 12.0856C30.1877 15.2348 29.6098 26.3641 18.9999 31.5036Z" fill="white" />
+                                        <path d="M17.4163 19.9281L13.7857 16.2975L11.5469 18.5364L17.4163 24.4058L26.4524 15.3697L24.2135 13.1309L17.4163 19.9281Z" fill="white" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
-                        <h2 className='text-2xl font-semibold py-6'>Best Materials</h2>
-                        <p className='font-medium text-lg leading-7 text-gray'>The material determines the
+                        <h2 className='text-2xl font-semibold py-6 max-sm:py-4 max-sm:text-xl'>Best Materials</h2>
+                        <p className='font-medium text-lg leading-7 text-gray max-sm:text-base'>The material determines the
                             building itself so we
                             recommend that you use
                             the best & quality materials
