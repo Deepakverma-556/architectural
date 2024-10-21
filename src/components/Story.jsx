c284365b162e43c4ad48f5e81903d613d091c709
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-21 12:48:05 +0530

    Remove all console error

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -11,13 +11,16 @@ const Story = () => {
                             <h2 className='text-xxxxl font-semibold leading-xxl max-w-[275px]'>
                                 Our Story Who we are
                             </h2>
-                            <p className='text-lg font-medium leading-7 text-gray pt-6 '>
+                            <p className='text-lg font-medium leading-7 text-gray pt-6'>
                                 Established in 1992, PT. Wahana Cipta operates as a General
                                 Contracting company with a footprint that we have planted
                                 throughout Indonesia. Initially, we focused on construction in
                                 the field of residential housing development in Jakarta.
                                 As the company grows, now we are present as a reliable...
                             </p>
+                            <button className="mt-12 py-5 px-10 font-semibold text-base leading-6 text-white bg-blue shadow-[0px_10px_15px_0px] [#01739933]">
+                                See More
+                            </button>
                        </div>
                     </div>
                     <div className='w-1/2 px-4'>
