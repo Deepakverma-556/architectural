b13c330a2859a457c38f914f6a71971952e45f87
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-21 12:50:59 +0530

    make responsive

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -6,7 +6,7 @@ const Story = () => {
         <div className='font-poppins'>
             <div className='max-w-xxl mx-auto px-4 pt-16 pb-16'>
                 <div className='flex flex-wrap flex-row -mx-3 '>
-                    <div className='w-1/2 px-4'>
+                    <div className='w-full lg:w-1/2 px-4'>
                         <div>
                             <h2 className='text-xxxxl font-semibold leading-xxl max-w-[275px]'>
                                 Our Story Who we are
@@ -21,10 +21,12 @@ const Story = () => {
                             <button className="mt-12 py-5 px-10 font-semibold text-base leading-6 text-white bg-blue shadow-[0px_10px_15px_0px] [#01739933]">
                                 See More
                             </button>
-                       </div>
+                        </div>
                     </div>
-                    <div className='w-1/2 px-4'>
-                        <img src={Image1} alt="building-image" className="border-[15px] border-white shadow-[0px_10px_15px_0px] shadow-[#9797971F]" />
+                    <div className='w-full lg:w-1/2 px-4'>
+                        <div className='max-lg:flex max-lg:justify-center'>
+                            <img src={Image1} alt="building-image" className="border-[15px] border-white shadow-[0px_10px_15px_0px] shadow-[#9797971F] max-lg:w-[700px]" />
+                        </div>
                     </div>
                 </div>
             </div>
