3266036eeb4e477f6361e743d47ce47b136d82fa
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-21 16:28:22 +0530

    add image

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -5,16 +5,32 @@ import Image3 from '../assets/images/sec-8-3-img.webp';
 import Image4 from '../assets/images/sec-8-4-img.webp'
 
 const Collection = () => {
-  return (
-      <div className='font-poppins'>
-          <div className='max-w-xxl mx-auto px-4'>
-              <h2 className='text-dark text-xxxxl font-semibold leading-xxl max-w-[326px] max-lg:mx-auto max-lg:max-w-none max-lg:text-4xl'>
-                  Our collection
-                  best project
-              </h2>
-          </div>
-    </div>
-  )
+    return (
+        <div className='font-poppins'>
+            <div className='max-w-xxl mx-auto px-4 pt-[55px] max-sm:pt-10 pb-20'>
+                <h2 className='text-dark text-xxxxl font-semibold leading-xxl max-w-[326px] max-lg:mx-auto max-lg:max-w-none max-lg:text-4xl max-lg:text-center'>
+                    Our collection
+                    best project
+                </h2>
+                <div className='flex flex-wrap flex-row -mx-3 pt-9 justify-center'>
+                    <div className='w-1/4 px-3'>
+                        <div>
+                            <img src={Image1} alt="building-image" />
+                            <div className='bg-white py-[22px] px-4 flex gap-7 items-center'>
+                                <div >
+                                    <p>Treasuru Tower</p>
+                                    <p>Project Completed</p>
+                                </div>
+                                <div className='w-[57px] h-[57px] bg-dark-blue'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Collection
