d3837503664a1e254a6e04e0d3775b798734cb03
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-21 10:35:36 +0530

    create new section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Excellent from './components/Excellent';
 import Header from './components/Header';
+import Make from './components/Make';
 
 function App() {
   return (
     <>
       <Header />
-      <Excellent/>
+      <Excellent />
+      <Make/>
     </>
   );
 }
---
+++ src/components/Make.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Make = () => {
+    return (
+        <div>
+            <div ma>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Make
