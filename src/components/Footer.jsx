d3da07dbe7003ef0a914267827299e9683802324
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-22 11:04:08 +0530

    add shadow on section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
         <div className='bg-gradient-to-r from-[#474747] to-[#222222] shadow-[0px_10px_15px_0px] shadow-[#9797971F]'>
             <div className='max-w-xxl mx-auto px-4'>
                 <div className='flex flex-wrap flex-row -mx-3'>
-                    <div className=''> 
+                    <div className='w-1/2 px-3'>
+                    
+                    </div>
+                    <div className='w-1/2 px-3'>
 
                     </div>
                 </div>
