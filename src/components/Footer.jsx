6eeae0bf06cc1a89ccf790af6535baf1fe61f2cd
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-22 11:03:18 +0530

    add section backgroud gradient color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div className='bg-gradient-to-r from-[#474747] to-[#222222] shadow-[0px_10px_15px_0px] shadow-[#9797971F]'>
+            <div className='max-w-xxl mx-auto px-4'>
+                <div className='flex flex-wrap flex-row -mx-3'>
+                    <div className=''> 
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
