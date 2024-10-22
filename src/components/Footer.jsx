54f1c845ec37f479aa6276eeeb23b2114956d217
Raghav-558 <jangraraghav9211@gmail.com>
2024-10-22 11:05:57 +0530

    add font family and use para tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,13 @@ const Footer = () => {
             <div className='max-w-xxl mx-auto px-4'>
                 <div className='flex flex-wrap flex-row -mx-3'>
                     <div className='w-1/2 px-3'>
-                    
+                        <div>
+                            <p className='font-poppins'>General Project</p>
+                            <p className='font-dm-sans'>is a general contractor company based in Jakarta. More than 25 years of experience in building and carving out Indonesia's development.</p>
+                            <div>
+
+                            </div>
+                        </div>
                     </div>
                     <div className='w-1/2 px-3'>
 
